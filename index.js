1e5ebede97b7eb2175521d61cbf90cd834da6f32
Meteologullo <angelologullo21@gmail.com>
2025-07-29 17:46:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,39 @@ const app = express();
 
 app.get("/", async (req, res) => {
   try {
+    console.log("Avvio browser Puppeteer...");
     const browser = await puppeteer.launch({
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
 
-    // Carica la pagina webcam Ecowitt
     await page.goto("https://www.ecowitt.net/home/index2?id=249399", {
       waitUntil: "networkidle2",
       timeout: 20000,
     });
 
-    // Estrai l'URL dell'immagine jpg dal DOM
     const imageUrl = await page.evaluate(() => {
       const imgs = Array.from(document.querySelectorAll('img'));
       const jpgImgs = imgs.filter(img => img.src && img.src.endsWith('.jpg'));
       return jpgImgs.length > 0 ? jpgImgs[0].src : null;
     });
 
-    await browser.close();
+    console.log("URL immagine estratta:", imageUrl);
 
     if (imageUrl) {
       res.redirect(imageUrl);
     } else {
       res.status(404).send("Nessuna immagine trovata.");
     }
+
+    // Non chiudere subito il browser, lascialo aperto per 10 secondi
+    setTimeout(async () => {
+      console.log("Chiusura browser Puppeteer...");
+      await browser.close();
+    }, 10000);
+
   } catch (err) {
+    console.error("Errore nell'elaborazione:", err);
     res.status(500).send("Errore: " + err.message);
   }
 });
