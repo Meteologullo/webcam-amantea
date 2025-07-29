050995e61dc0a95f12ca9f64ad5623f83dfca30b
Meteologullo <angelologullo21@gmail.com>
2025-07-29 16:15:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,22 @@ const app = express();
 app.get("/", async (req, res) => {
   try {
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      executablePath: '/usr/bin/chromium'
     });
     const page = await browser.newPage();
+
     await page.goto("https://www.ecowitt.net/home/index2?id=249399", {
       waitUntil: "networkidle2",
       timeout: 20000,
     });
+
     const imageUrl = await page.evaluate(() => {
       const imgs = Array.from(document.querySelectorAll('img'));
       const jpgImgs = imgs.filter(img => img.src && img.src.endsWith('.jpg'));
       return jpgImgs.length > 0 ? jpgImgs[0].src : null;
     });
+
     await browser.close();
 
     if (imageUrl) {
@@ -29,5 +33,5 @@ app.get("/", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log("Server avviato sulla porta " + port));
